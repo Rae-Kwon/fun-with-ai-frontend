b76caee20aa6b023b41d8d05989febb9ecf592ef
Richard Kwon <rkminjae@gmail.com>
2022-05-10 21:18:01 -0400

    disable eslint in next-env file

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,5 +1,7 @@
-import 'next';
-import 'next/image-types/global';
+/* eslint-disable spaced-comment */
+/* eslint-disable @typescript-eslint/triple-slash-reference */
+/// <reference types="next" />
+/// <reference types="next/image-types/global" />
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/basic-features/typescript for more information.
