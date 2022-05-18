eee4050f396991b677a5b4fb9bfe2a75a655d82c
Richard Kwon <rkminjae@gmail.com>
2022-05-18 19:53:25 -0400

    fix preset prompts to be more accessible

--- components/PresetPrompt.tsx
+++ components/PresetPrompt.tsx
@@ -1,14 +1,17 @@
-import type {FormEvent, KeyboardEvent} from 'react';
+import type {FormEvent, KeyboardEvent, MouseEvent} from 'react';
 
 import type {PresetPromptProps} from '../types';
 
 import styles from './PresetPrompt.module.css';
 
 const PresetPrompt = ({prompt, promptHandler, setOpen}: PresetPromptProps) => {
-  const prompOnKeyDownHandler = (
-    event: KeyboardEvent | FormEvent<HTMLButtonElement>,
+  const promptEventHandler = (
+    event: KeyboardEvent | MouseEvent | FormEvent<HTMLButtonElement>,
   ) => {
-    if ((event as KeyboardEvent).key === 'Enter') {
+    if (
+      (event as KeyboardEvent).key === 'Enter' ||
+      (event as MouseEvent).type === 'click'
+    ) {
       promptHandler(event as FormEvent<HTMLButtonElement>);
       setOpen(false);
     }
@@ -18,8 +21,8 @@ const PresetPrompt = ({prompt, promptHandler, setOpen}: PresetPromptProps) => {
     <button
       className={styles.presetPrompt}
       type="button"
-      onClick={(event) => promptHandler(event)}
-      onKeyDown={prompOnKeyDownHandler}
+      onClick={promptEventHandler}
+      onKeyDown={promptEventHandler}
     >
       {prompt}
     </button>
