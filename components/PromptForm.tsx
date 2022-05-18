c0b352acf7ac9e38af6bba7a89b2db6fc7dddf56
Richard Kwon <rkminjae@gmail.com>
2022-05-17 22:29:22 -0400

    add tooltip with animation for send icon

--- components/PromptForm.tsx
+++ components/PromptForm.tsx
@@ -34,6 +34,7 @@ const PromptForm = ({
         />
       </label>
       <label htmlFor="formSubmit" className={styles.sendButtonContainer}>
+        <span className={styles.tooltipText}>Send</span>
         <input
           aria-label="form submit"
           type="submit"
@@ -42,14 +43,16 @@ const PromptForm = ({
           value="Give me a song"
           className={styles.submitButton}
         />
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className={styles.sendButton}
-          viewBox="0 0 20 20"
-          fill="currentColor"
-        >
-          <path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z" />
-        </svg>
+        <div className={styles.sendButtonIconContainer}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className={styles.sendButton}
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z" />
+          </svg>
+        </div>
       </label>
     </form>
   );
